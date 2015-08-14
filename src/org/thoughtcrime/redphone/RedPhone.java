4dc8db7f1abf54d21f52619a4a01697128fde8b2
Tilo Klausing <klti@klti.de>
2015-08-14 18:05:37 +0200

    RedPhone activity will be displayed over the lockscreen (for example when ringing)
    makes it possible to answer calls without having to unlock your phone first, just as it works for regular calls

--- src/org/thoughtcrime/redphone/RedPhone.java
+++ src/org/thoughtcrime/redphone/RedPhone.java
@@ -115,6 +115,10 @@ public class RedPhone extends Activity {
 
     startServiceIfNecessary();
     requestWindowFeature(Window.FEATURE_NO_TITLE);
+    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON|
+            WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD|
+            WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED|
+            WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
     setContentView(R.layout.main);
 
     setVolumeControlStream(AudioManager.STREAM_VOICE_CALL);
